242e676b0c73e4f68285b903a9a67a515c8d502f
jhonSsosa <112003191+jhonSsosa@users.noreply.github.com>
2024-02-09 21:51:41 -0500

    Add files via upload

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/src/RegularShapeType.java
@@ -0,0 +1,4 @@
+package edu.eci.cvds.patterns.shapes.src;
+public enum RegularShapeType {
+    Triangle, Quadrilateral, Pentagon, Hexagon
+}
---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/src/Shape.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.patterns.shapes.src;
+
+public class Triangle implements Shape {
+    public int getNumberOfEdges(){
+        return 3;
+    }
+}
